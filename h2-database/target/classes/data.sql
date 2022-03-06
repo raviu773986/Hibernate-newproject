d4e22d115b78653201bfb746c46068a04a002742
raviu773986 <55343355+raviu773986@users.noreply.github.com>
2022-03-06 23:49:14 +0530

    Project submission

---
+++ h2-database/target/classes/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO 
+	TBL_EMPLOYEES (first_name, last_name, email, salary, designation) 
+VALUES
+  	('Mukesh', 'Yadav', 'myl16@gmail.com', 12000, 'ASE'),
+  	('Shyam', 'pandet', 'shyam4@gmail.com', 2000, 'ATE'),
+  	('Ravi', 'Kumar', 'ravikumar321@gmail.com', 15000, 'LEAD');
+  	
